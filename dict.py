e0f1996150847db8790ec47b5a75a6f52032e3bb
meolp <olchik.melnyk@gmail.com>
2022-02-06 23:04:25 +0200

    add files

---
+++ dict.py
@@ -0,0 +1,11 @@
+#
+my_list = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6, 7, 7, 7, 7, 7,1]
+# наприклад порахувати скільки разів в списку зустрічається елемент 5
+count_dict = {}
+# створимо словник для підрахунку
+for element in my_list:
+  if element in count_dict:
+      count_dict[element]+=1
+  else:
+      count_dict[element]=1
+print(count_dict)
